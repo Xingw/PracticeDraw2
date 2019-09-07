7524fce3dc973d7ac3aa7a31ffbda8746cb9e766
xingwei <xingwei@soundai.com>
2019-09-07 10:37:45 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
@@ -2,7 +2,9 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -22,15 +24,18 @@ public class Practice03SweepGradientView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    SweepGradient sweepGradient;
     {
         // 用 Paint.setShader(shader) 设置一个 SweepGradient
         // SweepGradient 的参数：圆心坐标：(300, 300)；颜色：#E91E63 到 #2196F3
+        sweepGradient = new SweepGradient(300,300, Color.parseColor("#E91E63"),Color.parseColor("#2196F3"));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setShader(sweepGradient);
         canvas.drawCircle(300, 300, 200, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
@@ -1,11 +1,20 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
+import android.graphics.Xfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice05ComposeShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -22,18 +31,29 @@ public class Practice05ComposeShaderView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    BitmapShader bitmapShader;
+    BitmapShader bitmapShader2;
+    Shader shader;
+
     {
         setLayerType(LAYER_TYPE_SOFTWARE, null); // 硬件加速下 ComposeShader 不能使用两个同类型的 Shader
 
         // 用 Paint.setShader(shader) 设置一个 ComposeShader
         // Shader 1: BitmapShader 图片：R.drawable.batman
         // Shader 2: BitmapShader 图片：R.drawable.batman_logo
+        Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(), R.drawable.batman_logo);
+        bitmapShader = new BitmapShader(bitmap1, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        bitmapShader2 = new BitmapShader(bitmap2, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        shader = new ComposeShader(bitmapShader,bitmapShader2, PorterDuff.Mode.DST_IN);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setShader(shader);
         canvas.drawCircle(200, 200, 200, paint);
     }
 }
